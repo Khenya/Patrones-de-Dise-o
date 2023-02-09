f95f7520fda545f8e1746b5b64093102d65c9726
Khenya <abrendakhenya@gmail.com>
2023-02-09 17:37:14 -0400

    Logger

--- Logger.java
+++ Logger.java
@@ -2,42 +2,33 @@ import java.io.*;
 
 public class Logger {
     private static Logger instance = null;
-    private File file = new File("C:\\Users\\Ambar\\Documents\\JavaProyects\\Patrones2023\\src\\main\\java\\Practices\\logger\\text.txt");
+    private File file = new File("B:\\Patrones de diseño\\Logger\\text.txt");
     private FileWriter write;
     private PrintWriter line;
     String name = "";
 
+
     private Logger() throws IOException {
-        System.out.println("Se inicia el registro");
-
-        write = new FileWriter(file, true);
+        System.out.println("Iniciar Registro");
         line = new PrintWriter(write);
-        line.println("---> Inicio de registro <---");
-
-
+        line.println("Se INICIO EL REGISTRO");
     }
-
     private synchronized static void create() throws IOException {
-        if (instance == null)
+        if (instance == null){
             instance = new Logger();
+        }
     }
-
     public static Logger getInstance() throws IOException {
-        if (instance == null)
+        if (instance == null){
             create();
+        }
         return instance;
     }
-
     public synchronized void addRegister(String register) throws IOException {
-
         line.println(register);
-
     }
-
     public void finish() throws IOException {
-        System.out.println("Se finalizó el registro");
-
-        line.println("---> Fin de Registro <---" );
+        System.out.println("Registro Finalisado");
         line.close();
         write.close();
     }
